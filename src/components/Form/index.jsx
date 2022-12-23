d800a62672d47b3f9263efa08478180db571db13
abderrahmen lahmedi <abderrahmen.lahmedi@ensit.u-tunis.tn>
2022-12-23 13:15:31 +0100

    Update index.jsx

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 function FileUploadSingle() {
   const [file, setFile] = useState();
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
 
   const convertBase64 = (file) => {
     return new Promise((resolve, reject) => {
@@ -30,15 +32,19 @@ function FileUploadSingle() {
     try {
       const base64 = await convertBase64(file);
      
-      const data = await fetch('<ekteb url mtea API lena', {
+      const data = await fetch('http://localhost:5000/music', {
         method: 'POST',
-        body: base64,
+        credentials: 'same-origin',
+        body: JSON.stringify({music:base64}),
         headers: {
-          'content-type': file.type,
-          'content-length': `${file.size}`,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'access-control-allow-origin' : ['http://localhost:5000','http://localhost:5000/music']
         },
       })
-      console.log(data?.json());
+      const genre = await data.text();
+      setSuccess(genre);
+      setError(error);
     } catch (error) {
       console.error(error);
     }
@@ -48,6 +54,9 @@ function FileUploadSingle() {
     <>
       <input type="file"  aria-label="Recipient's username" aria-describedby="button-addon2" className="form-control" onChange={handleFileChange} />
       <button className="btn btn-success border-rad" type="button" id="button-addon2" onClick={handleUploadClick}>Upload</button>
+      <div className="break" ></div>
+      {error && <p className="mt-4 font-14 text-danger">{error}</p>}
+      {success && <p className="mt-4 font-14 text-success">{success}</p>}
     </>
   );
 }
